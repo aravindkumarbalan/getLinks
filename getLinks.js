89997fdb19e95d465845451f3ea115b0cfbf2ee7
aravindkumarbalan <aravindkumar.b22@gmail.com>
2022-11-20 14:45:09 +0530

    removed some init codes

--- getLinks.js
+++ getLinks.js
@@ -3,10 +3,6 @@ var request = require('request');
 var url = require('url');
 var cheerio = require('cheerio');
 const server = http.createServer();
-var arrLinks = []
-arrLinks.push('http://entrepreneur.com/');
-var crawledUrl = {'/':arrLinks};
-console.log(crawledUrl);
 
 server.on('request', async (req, res) => {
 	res.statusCode = 200;
@@ -15,38 +11,20 @@ server.on('request', async (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 	res.setHeader('Content-Type', 'text/json');
-	var mainUrl = false;
-
 		var url_parts = url.parse(req.url, true);
 		var query = url_parts.query;
 		if(url_parts.query.url != null) {
-		 
-		  
-			  
-				   try {
-				
-					  let response_body = await getPromise(query);
-					  res.end(JSON.stringify(response_body));
-				   }catch (err) {
-					 let response_body = [];
-					 res.end(JSON.stringify(response_body));
-				  }
-		
-		
-	 
+			try {
+				let response_body = await getPromise(query);
+				res.end(JSON.stringify(response_body));
+			}catch (err) {
+				let response_body = [];
+				res.end(JSON.stringify(response_body));
+			}
 		}
-	//});
-	
-  
 });
 
-function someAsyncFunc() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve({data: 1});
-        }, 1000);
-    });
-}
+
 var webLinks = [];
 function getPromise(websiteLink) {
 	console.log(websiteLink);
@@ -57,11 +35,11 @@ function getPromise(websiteLink) {
                 reject('Invalid status code <' + resp.statusCode + '>');
             }
 			$ = cheerio.load(body);
-			  links = $('a');
-			  $(links).each(function(i, link){
-				var href = $(link).attr('href');
-				webLinks.push(href);
-			  });
+			links = $('a');
+			$(links).each(function(i, link){
+			var href = $(link).attr('href');
+			webLinks.push(href);
+			});
 
             resolve(webLinks);
 			  
@@ -69,8 +47,6 @@ function getPromise(websiteLink) {
 	});
 }
 
-//server.listen(3000);
-
 server.listen('3000', 'localhost', () => {
-		  console.log(`Server running at http://localhost:3000/`);
-		});
+	console.log(`Server running at http://localhost:3000/`);
+});
